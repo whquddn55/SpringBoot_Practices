4daa40a42776c2e1d10124963f1bac8548beb62e
thuthi <whquddn55@gmail.com>
2023-04-08 18:06:23 +0900

    feat: Member 도메인 메모리 버전 기능 구현

---
+++ src/main/java/com/thuthi/core/member/Grade.java
@@ -0,0 +1,6 @@
+package com.thuthi.core.member;
+
+public enum Grade {
+    BASIC,
+    VIP
+}
---
+++ src/main/java/com/thuthi/core/member/MemberRepository.java
@@ -0,0 +1,6 @@
+package com.thuthi.core.member;
+
+public interface MemberRepository {
+    void save(Member member);
+    Member findById(Long memberId);
+}
