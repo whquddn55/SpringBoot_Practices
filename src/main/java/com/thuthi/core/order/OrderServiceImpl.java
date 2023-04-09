98bbb147ae73d21728c578f851cd48fe13432f45
thuthi <whquddn55@gmail.com>
2023-04-09 13:38:38 +0900

    feat: RateDiscountPolicy 구현

--- src/main/java/com/thuthi/core/order/OrderServiceImpl.java
+++ src/main/java/com/thuthi/core/order/OrderServiceImpl.java
@@ -2,13 +2,16 @@ package com.thuthi.core.order;
 
 import com.thuthi.core.discount.DiscountPolicy;
 import com.thuthi.core.discount.FixDiscountPolicy;
+import com.thuthi.core.discount.RateDiscountPolicy;
 import com.thuthi.core.member.Member;
 import com.thuthi.core.member.MemberRepository;
 import com.thuthi.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
