a9a4fb79a362d14f76eb1149b1f362646f184b2d
thuthi <whquddn55@gmail.com>
2023-04-09 21:41:00 +0900

    refactor: AppConfig 중복 코드 제거

--- src/main/java/com/thuthi/core/AppConfig.java
+++ src/main/java/com/thuthi/core/AppConfig.java
@@ -1,7 +1,9 @@
 package com.thuthi.core;
 
+import com.thuthi.core.discount.DiscountPolicy;
 import com.thuthi.core.discount.FixDiscountPolicy;
 import com.thuthi.core.discount.RateDiscountPolicy;
+import com.thuthi.core.member.MemberRepository;
 import com.thuthi.core.member.MemberService;
 import com.thuthi.core.member.MemberServiceImpl;
 import com.thuthi.core.member.MemoryMemberRepository;
@@ -9,11 +11,19 @@ import com.thuthi.core.order.OrderService;
 import com.thuthi.core.order.OrderServiceImpl;
 
 public class AppConfig {
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
+    }
+
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 }
