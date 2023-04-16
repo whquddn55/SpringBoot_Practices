e97e2777b2aea000c259206e0210d19881b34bc8
thuthi <whquddn55@gmail.com>
2023-04-16 15:41:08 +0900

    chore: Lombok 라이브버리 추가 및 적용

--- src/main/java/com/thuthi/core/order/OrderServiceImpl.java
+++ src/main/java/com/thuthi/core/order/OrderServiceImpl.java
@@ -3,19 +3,14 @@ package com.thuthi.core.order;
 import com.thuthi.core.discount.DiscountPolicy;
 import com.thuthi.core.member.Member;
 import com.thuthi.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
