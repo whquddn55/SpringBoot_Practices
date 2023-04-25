83510cf8e199382ba75bec00413ab2b9fa357800
thuthi <whquddn55@gmail.com>
2023-04-25 22:13:09 +0900

    test: 프로토타입 빈 테스트 코드 추가

---
+++ src/test/java/scope/PrototypeTest.java
@@ -0,0 +1,39 @@
+package scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class PrototypeTest {
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        System.out.println("find prototypeBean1");
+        PrototypeBean bean1 = ac.getBean(PrototypeBean.class);
+
+        System.out.println("find prototypeBean2");
+        PrototypeBean bean2 = ac.getBean(PrototypeBean.class);
+        System.out.println("bean1 = " + bean1);
+        System.out.println("bean2 = " + bean2);
+        assertThat(bean1).isNotSameAs(bean2);
+
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+}
