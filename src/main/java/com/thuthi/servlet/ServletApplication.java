95ae5b4f99ebf90677a77ff9136898ba5523122b
thuthi <whquddn55@gmail.com>
2023-04-28 08:47:16 +0900

    feat: HelloServlet 등록 및 기본 기능 사용 예제 추가

--- src/main/java/com/thuthi/servlet/ServletApplication.java
+++ src/main/java/com/thuthi/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package com.thuthi.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan // 하위 패키지의 서블릿 자동 등록
 @SpringBootApplication
 public class ServletApplication {
 
