3bcdf1abc8e6f4949a07637456c7fb64a257a371
thuthi <whquddn55@gmail.com>
2023-05-05 19:23:54 +0900

    feat: Model 추가 - v3

--- src/main/java/com/thuthi/servlet/web/frontcontroller/MyView.java
+++ src/main/java/com/thuthi/servlet/web/frontcontroller/MyView.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
     private String viewPath;
@@ -16,5 +17,14 @@ public class MyView {
     public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        modelToRequestAttribute(model, request);
+        render(request, response);
+    }
+
+    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+        model.forEach(request::setAttribute);
     }
 }
