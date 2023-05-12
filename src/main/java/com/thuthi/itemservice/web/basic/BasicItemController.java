7ccd97c6aaf2fee59d1f9d5cec4e75b0c56351e9
thuthi <whquddn55@gmail.com>
2023-05-13 08:00:26 +0900

    feat: 상품 리스트 조회 뷰, 컨트롤러 추가

---
+++ src/main/java/com/thuthi/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,35 @@
+package com.thuthi.itemservice.web.basic;
+
+import com.thuthi.itemservice.domain.item.Item;
+import com.thuthi.itemservice.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+        itemRepository.save(new Item("itemC", 30000, 30));
+    }
+}
